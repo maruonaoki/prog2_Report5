2096b5d162a3b20389e57c6cd0fffc05011699d9
Maruo Naoki <e235712@ie.u-ryukyu.ac.jp>
2023-12-11 21:26:38 +0900

    step2

--- Report5.java
+++ Report5.java
@@ -1,6 +1,14 @@
 public class Report5 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length()); 
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            // 条件1
+            System.out.println("NullPointerExceptionが発生しました。");
+
+            // 条件3
+            System.out.println("例外メッセージ: " + e.getMessage());
+        }
     }
 }
